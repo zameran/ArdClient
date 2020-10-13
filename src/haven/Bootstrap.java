8dc63794c6dda71b23417a3f63827a686a7c9c69
Cediner <cedivargardner@gmail.com>
2020-10-13 10:29:58 +0300

    npe raise

--- src/haven/Bootstrap.java
+++ src/haven/Bootstrap.java
@@ -73,7 +73,7 @@ public class Bootstrap implements UI.Receiver, UI.Runner {
     public Session run(UI ui) throws InterruptedException {
         ui.setreceiver(this);
         ui.bind(ui.root.add(new LoginScreen()), 1);
-        ui.root.sessionDisplay.raise();
+        //ui.root.sessionDisplay.raise();
         String loginname = getpref("loginname", "");
         boolean savepw = false;
         byte[] token = null;
