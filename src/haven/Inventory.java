3534b8687961ef9314ead21b3414929492aed9cf
Cediner <cedivargardner@gmail.com>
2020-12-04 03:46:40 +0300

    fix max quality color transfer

--- src/haven/Inventory.java
+++ src/haven/Inventory.java
@@ -203,15 +203,13 @@ public class Inventory extends Widget implements DTarget {
                                     if (colorIdentical == null) {
                                         colorIdentical = g.quality().color;
                                     }
-                                    if (item.qq.color != colorIdentical) {
+                                    if (!item.qq.color.equals(colorIdentical)) {
                                         continue;
                                     }
                                 }
                             }
-                            item.item.wdgmsg("transfer", Coord.z);
-                        } else {
-                            item.item.wdgmsg("transfer", Coord.z);
                         }
+                        item.item.wdgmsg("transfer", Coord.z);
                     } catch (Exception e) {
                         System.out.println(e.getMessage());
                     }
